d9000a4fb3511a63a3ea2695a2c7a98531bbf91f
Mohaiminul375 <mohaiminul375@gmail.com>
2024-10-10 19:41:55 +0600

    crate navbar

---
+++ src/app/Components/Navbar.tsx
@@ -0,0 +1,140 @@
+"use client"
+import { NavbarBrand, NavbarContent, NavbarItem, Link, Navbar, NavbarMenuToggle, NavbarMenuItem, NavbarMenu } from "@nextui-org/react";
+import Image from "next/image";
+import { Kalam } from '@next/font/google';
+import React from "react";
+// import path from "path";
+
+interface NavItems {
+    title: string,
+    path: string,
+}
+
+
+// font
+const kalam = Kalam({
+    weight: '700',
+    subsets: ['latin'],
+});
+const navItems: NavItems[] = [
+    {
+        title: "Home",
+        path: "/",
+    },
+    {
+        title: "Products",
+        path: "/all-products",
+    },
+
+]
+export default function Nav() {
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+    return (
+        <Navbar
+            isMenuOpen={isMenuOpen}
+            onMenuOpenChange={setIsMenuOpen}
+            isBordered maxWidth="full"
+            className="md:px-20">
+            <NavbarContent className="md:hidden" justify="start">
+                <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
+            </NavbarContent>
+            {/* <NavbarContent justify="start"> */}
+                <NavbarBrand>
+                    <div className="flex items-center gap-3 justify-start">
+                        <Image src='/assets/logo.webp' alt="logo" height={40} width={40} />
+                        <h2 className={`md:text-3xl text-[#36454F] ${kalam.className}`}>Electro-Hub</h2>
+                    </div>
+                </NavbarBrand>
+            {/* </NavbarContent> */}
+            <NavbarContent className="hidden md:flex gap-4" justify="center">
+
+                {
+                    navItems?.map(({ title, path }, idx) => (
+                        <NavbarItem key={idx}>
+                            <Link color="foreground" href={path}>
+                                {title}
+                            </Link>
+                        </NavbarItem>
+                    ))
+                }
+            </NavbarContent>
+            <NavbarContent justify="end">
+                <NavbarItem className="flex">
+                    <Link className="bg-[#36454F] p-2 rounded-full text-white px-3" href="/login">Login</Link>
+                </NavbarItem>
+            </NavbarContent>
+            {/* menu for sm device */}
+            <NavbarMenu>
+                {navItems.map(({ title, path }, idx) => (
+                    <NavbarMenuItem
+                        key={idx}>
+                        <Link
+                            className="w-full"
+                            color={
+                                idx === 2 ? "warning" : idx === navItems.length - 1 ? "danger" : "foreground"
+                            }
+                            href={path}
+                            size="lg"
+                        >
+                            {title}
+                        </Link>
+                    </NavbarMenuItem>
+                ))}
+            </NavbarMenu>
+        </Navbar>
+        // <Navbar
+        //     isBordered
+        //     maxWidth="full"
+        // isMenuOpen={isMenuOpen}
+        // onMenuOpenChange={setIsMenuOpen}
+        // >
+        //     <NavbarContent className="sm:hidden" justify="start">
+        //         <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
+        //     </NavbarContent>
+
+        //     <NavbarContent className="sm:hidden pr-3" justify="center">
+        //         <NavbarBrand>
+        //             {/* <AcmeLogo /> */}
+        //             <p className="font-bold text-inherit">ACME</p>
+        //         </NavbarBrand>
+        //     </NavbarContent>
+
+        //     <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        //         <NavbarBrand>
+        //             {/* <AcmeLogo /> */}
+        //             <p className="font-bold text-inherit">ACME</p>
+        //         </NavbarBrand>
+
+        //     </NavbarContent>
+
+        //     <NavbarContent justify="end">
+        //         <NavbarItem className="hidden lg:flex">
+        //             <Link href="#">Login</Link>
+        //         </NavbarItem>
+        //         <NavbarItem>
+        //             <Button as={Link} color="warning" href="#" variant="flat">
+        //                 Sign Up
+        //             </Button>
+        //         </NavbarItem>
+        //     </NavbarContent>
+
+        //     <NavbarMenu>
+        //         {navItems.map(({ title, path }, idx) => (
+        //             <NavbarMenuItem
+        //                 key={idx}>
+        //                 <Link
+        //                     className="w-full"
+        //                     color={
+        //                         idx === 2 ? "warning" : idx === navItems.length - 1 ? "danger" : "foreground"
+        //                     }
+        //                     href={path}
+        //                     size="lg"
+        //                 >
+        //                     {title}
+        //                 </Link>
+        //             </NavbarMenuItem>
+        //         ))}
+        //     </NavbarMenu>
+        // </Navbar>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-// import Image from "next/image";
-
-import { Button } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="">
       <div>
-        <Button color="primary">
-          Button
-        </Button>
+        
       </div>
     </div>
   );
