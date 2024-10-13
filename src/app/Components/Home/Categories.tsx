66092a7e95943a17a480cb338e9daec137de5a93
Mohaiminul375 <mohaiminul375@gmail.com>
2024-10-13 13:33:03 +0600

    creqate 6 categories

--- src/app/Components/Home/Categories.tsx
+++ src/app/Components/Home/Categories.tsx
@@ -1,10 +1,76 @@
+"use client"
+import Link from 'next/link'
 import React from 'react'
+import { BsSmartwatch } from 'react-icons/bs'
+import { FaLaptop } from 'react-icons/fa'
+import { FaComputerMouse } from 'react-icons/fa6'
+import { GiSmartphone } from 'react-icons/gi'
+import { LuMonitor } from 'react-icons/lu'
+import { MdTv } from 'react-icons/md'
 
 export default function Categories() {
     return (
         <section className='mt-10'>
-            <div className=''>
-                <h3 className='text-center text-4xl text-accent'>This is Categories</h3>
+            <div>
+                <h2 className='text-3xl font-semibold'>Categories</h2>
+            </div>
+            {/* categories */}
+
+            <div className='grid lg:grid-cols-6 mt-5 gap-16'>
+                {/* smart phone */}
+                <Link href='/category/smart-phone'>
+                    <div className='border hover:border-none w-40 flex flex-col items-center p-3 bg-white space-y-2 hover:shadow border-primary rounded-md shadow-2xl lg:transition lg:hover:scale-105 group'>
+                        <GiSmartphone className='text-5xl font-bold' />
+                        <div>
+                            <h2 className='text-xl group-hover:text-accent duration-300'>Smart Phone</h2>
+                        </div>
+                    </div>
+                </Link>
+                {/* smart Watch */}
+                <Link href='/category/smart-watch'>
+                    <div className='border hover:border-none w-40 flex flex-col items-center p-3 bg-white space-y-2 hover:shadow border-primary rounded-md shadow-2xl lg:transition lg:hover:scale-105 group'>
+                        <BsSmartwatch className='text-5xl' />
+                        <div>
+                            <h2 className='text-xl group-hover:text-accent duration-300'>Smart Watch</h2>
+                        </div>
+                    </div>
+                </Link>
+                {/* laptop */}
+                <Link href='/category/laptop'>
+                    <div className='border hover:border-none w-40 flex flex-col items-center p-3 bg-white space-y-2 hover:shadow border-primary rounded-md shadow-2xl lg:transition lg:hover:scale-105 group'>
+                        <FaLaptop className='text-5xl font-bold' />
+                        <div>
+                            <h2 className='text-xl group-hover:text-accent duration-300'>Laptop</h2>
+                        </div>
+                    </div>
+                </Link>
+                {/* monitor */}
+                <Link href='/category/smart-phone'>
+                    <div className='border hover:border-none w-40 flex flex-col items-center p-3 bg-white space-y-2 hover:shadow border-primary rounded-md shadow-2xl lg:transition lg:hover:scale-105 group'>
+                    <LuMonitor className='text-5xl font-bold' />
+                        <div>
+                            <h2 className='text-xl group-hover:text-accent duration-300'>Monitor</h2>
+                        </div>
+                    </div>
+                </Link>
+                {/* accessories */}
+                <Link href='/category/accessories'>
+                    <div className='border hover:border-none w-40 flex flex-col items-center p-3 bg-white space-y-2 hover:shadow border-primary rounded-md shadow-2xl lg:transition lg:hover:scale-105 group'>
+                        <FaComputerMouse className='text-5xl font-bold' />
+                        <div>
+                            <h2 className='text-xl group-hover:text-accent duration-300'>Accessories</h2>
+                        </div>
+                    </div>
+                </Link>
+                {/* smart tv */}
+                <Link href='/category/smart-tv'>
+                    <div className='border hover:border-none w-40 flex flex-col items-center p-3 bg-white space-y-2 hover:shadow border-primary rounded-md shadow-2xl lg:transition lg:hover:scale-105 group'>
+                        <MdTv className='text-5xl font-bold' />
+                        <div>
+                            <h2 className='text-xl group-hover:text-accent duration-300'>Smart TV</h2>
+                        </div>
+                    </div>
+                </Link>
             </div>
         </section>
     )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,9 +36,11 @@ export default function RootLayout({
         <NextUIProvider>
           <AuthProvider>
             <Nav />
-            <div className="min-h-[calc(100vh-145px)] bg-background">
-              {children}
-            </div>
+            <main className=" bg-background ">
+              <div className="min-h-[calc(100vh-145px)] md:max-w-7xl mx-auto">
+                {children}
+              </div>
+            </main>
             <Footer />
           </AuthProvider>
           <Toaster position="top-center" />
