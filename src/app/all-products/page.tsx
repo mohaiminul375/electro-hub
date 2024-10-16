41fd24a421e4b759212b9d56fc9b5e461127f17a
Mohaiminul375 <mohaiminul375@gmail.com>
2024-10-16 20:07:06 +0600

    update code

--- src/app/all-products/page.tsx
+++ src/app/all-products/page.tsx
@@ -1,9 +1,9 @@
-import { getAllProduct } from "./rotute";
+import { getAllProduct } from "./route";
 
 
 const page = async () => {
     const products: [] = await getAllProduct();
-    console.log(products)
+    // console.log(products)
     return (
         <div>
             {products.length}
