02e61ca64c30f08b2f01a38a5c681b7bd130f10b
Mohaiminul375 <mohaiminul375@gmail.com>
2024-11-26 18:58:07 +0600

    delete product

--- src/app/Components/Dashboard/ProductTable/ProductTable.tsx
+++ src/app/Components/Dashboard/ProductTable/ProductTable.tsx
@@ -1,17 +1,40 @@
 'use client'
+import { deleteProduct } from '@/app/admin-dashboard/manage-product/api/rote';
 import { useDisclosure } from '@nextui-org/react';
 import Link from 'next/link';
 import { FaEye, FaTrash } from 'react-icons/fa';
 import { FaPencil } from 'react-icons/fa6';
+import Swal from 'sweetalert2';
 
 const ProductTable = ({ product, idx }) => {
     const { _id, product_name, category, brand } = product;
-    // // modal controlar
-    // const {isOpen, onOpen, onClose} = useDisclosure();
-    // const handleOpen = (size) => {
-    //     onOpen();
-    //   }
 
+    // delete product
+    const handleDeleteProduct = async (id: string) => {
+        console.log(id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                const res = await deleteProduct(id);
+                console.log(res)
+                if (res.deletedCount == 1) {
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                    });
+                }
+
+            }
+        });
+    }
 
 
     return (
@@ -28,7 +51,9 @@ const ProductTable = ({ product, idx }) => {
                     // onClick={() => handleDelete(_id)}
                     className="flex gap-3 ml-2 text-red-600 hover:text-red-900">
                     <Link href={`product-details/${_id}`}>  <FaEye title="view details" className="text-white bg-primary rounded-full text-3xl p-2 " /></Link>
-                    <FaTrash title="delete user" className="text-white bg-red-700 rounded-full text-3xl p-2 " />
+                    <FaTrash
+                        onClick={() => handleDeleteProduct(_id)}
+                        title="delete user" className="text-white bg-red-700 rounded-full text-3xl p-2 " />
                 </button>
             </td>
         </tr>
--- src/app/admin-dashboard/manage-product/api/rote.ts
+++ src/app/admin-dashboard/manage-product/api/rote.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+// get products for admin
 export const getAdminProducts = async () => {
     try {
         const response = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/all-products-admin`)
@@ -10,4 +10,16 @@ export const getAdminProducts = async () => {
         throw error;
     }
 
+}
+
+// delete a product
+export const deleteProduct = async (id: string) => {
+    try {
+        const response = await axios.delete(`${process.env.NEXT_PUBLIC_SERVER_URL}/all-products-admin/${id}`)
+        return response.data;
+    }
+
+    catch (error) {
+        throw error;
+    }
 }
