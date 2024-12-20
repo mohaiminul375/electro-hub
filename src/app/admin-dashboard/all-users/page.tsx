65c6859916b3a1944b2eb4daf44406b25050cf29
Mohaiminul375 <mohaiminul375@gmail.com>
2024-12-20 21:02:12 +0600

    update conditintion

--- src/app/admin-dashboard/all-users/page.tsx
+++ src/app/admin-dashboard/all-users/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
         setIsClient(true);
     }, []);
 
-    if (!isClient) {
+    if (typeof window === "undefined") {
         return null;
     }
 
@@ -24,52 +24,56 @@ export default function Page() {
     if (isError) return <p className="text-center text-red-700">Error: {error && (typeof error === "string" ? error : error.message)}</p>;
 
     return (
-        <section>
-            <div className="mt-6">
-                {/* Back Button */}
-                <div className="inline-block">
-                    <Link
-                        href="/admin-dashboard"
-                        className="flex items-center gap-3 rounded-lg bg-white border border-gray-400 text-lg px-4 py-2 text-accent hover:bg-gray-300 hover:border-gray-500 transition duration-200"
-                    >
-                        <FaArrowLeft className="text-accent" />
-                        <span>Back to Dashboard</span>
-                    </Link>
-                </div>
+        <>
+            {
+                isClient && <section>
+                    <div className="mt-6">
+                        {/* Back Button */}
+                        <div className="inline-block">
+                            <Link
+                                href="/admin-dashboard"
+                                className="flex items-center gap-3 rounded-lg bg-white border border-gray-400 text-lg px-4 py-2 text-accent hover:bg-gray-300 hover:border-gray-500 transition duration-200"
+                            >
+                                <FaArrowLeft className="text-accent" />
+                                <span>Back to Dashboard</span>
+                            </Link>
+                        </div>
 
-                {/* Registered Users Header */}
-                <div className="mb-10 mt-4 text-center">
-                    <h3 className="text-3xl font-bold text-accent">Registered Users</h3>
-                    <h4 className="text-md text-gray-700 mt-2">List of all registered users on the platform.</h4>
-                </div>
-            </div>
+                        {/* Registered Users Header */}
+                        <div className="mb-10 mt-4 text-center">
+                            <h3 className="text-3xl font-bold text-accent">Registered Users</h3>
+                            <h4 className="text-md text-gray-700 mt-2">List of all registered users on the platform.</h4>
+                        </div>
+                    </div>
 
-            {/* table */}
-            <div className="overflow-x-auto">
-                <table className="min-w-full table-auto border-collapse">
-                    <thead className="bg-primary text-white">
-                        <tr>
-                            <th className="px-4 py-2 text-left">#</th>
-                            <th className="px-4 py-2 text-left">Name</th>
-                            <th className="px-4 py-2 text-left">Email</th>
-                            <th className="px-4 py-2 text-left">Role</th>
-                            <th className="px-4 py-2 text-left">Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            users?.map((user, idx) => (
-                                <UserTable
-                                    idx={idx}
-                                    key={idx}
-                                    user={user}
-                                />
-                            ))
-                        }
-                    </tbody>
-                </table>
-            </div>
-        </section>
+                    {/* table */}
+                    <div className="overflow-x-auto">
+                        <table className="min-w-full table-auto border-collapse">
+                            <thead className="bg-primary text-white">
+                                <tr>
+                                    <th className="px-4 py-2 text-left">#</th>
+                                    <th className="px-4 py-2 text-left">Name</th>
+                                    <th className="px-4 py-2 text-left">Email</th>
+                                    <th className="px-4 py-2 text-left">Role</th>
+                                    <th className="px-4 py-2 text-left">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    users?.map((user, idx) => (
+                                        <UserTable
+                                            idx={idx}
+                                            key={idx}
+                                            user={user}
+                                        />
+                                    ))
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </section>
+            }
+        </>
 
     )
 }
