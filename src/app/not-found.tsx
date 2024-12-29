40895cc51853aabb966766b7036c84828b2ee821
Mohaiminul375 <mohaiminul375@gmail.com>
2024-12-29 20:08:17 +0600

    resolving issue

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,7 +10,7 @@ const Error = () => {
         <div className="flex flex-col justify-center items-center h-screen">
             <div className="bg-white p-6 rounded-lg shadow-md text-center">
                 <h1 className="text-2xl font-bold text-red-500">Oops! Something went wrong.</h1>
-                <p className="text-gray-600 mt-2">We couldn't find the page you're looking for.</p>
+                <p className="text-gray-600 mt-2">We couldn&apos;t find the page&apos;looking for.</p>
                 <button
                     onClick={() => router.push('/')}
                     className="mt-4 px-4 py-2 bg-primary text-white rounded hover:rounded-2xl duration-300 transition-all"
