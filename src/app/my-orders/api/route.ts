e638da171ecafda565837fefc6877c629d049830
Mohaiminul375 <mohaiminul375@gmail.com>
2025-01-13 12:36:10 +0600

    worked on typescript

--- src/app/my-orders/api/route.ts
+++ src/app/my-orders/api/route.ts
@@ -1,8 +1,38 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+interface Address {
+    division: string;
+    district: string;
+    full_address: string;
+}
+interface Products {
+    product_id: string;
+    img: string;
+    product_name: string;
+    color: string;
+    brand: string;
+    price: number;
+    quantity: number;
+    category: string;
+}
+interface Orders {
+    _id: string;
+    customer_uuid: string;
+    customer_name: string;
+    customer_email: string;
+    customer_Phone: string;
+    transaction_id: string;
+    total_price: number;
+    address: Address;
+    orderCreatedAt: string;
+    order_id: string;
+    order_status: string;
+    payment_method: string;
+    products: Products[];
+}
 // Get All Products for users
 export const useAllProductsUser = (uuid: string) => {
-    const { data, isLoading, error, isError } = useQuery({
+    const { data, isLoading, error, isError } = useQuery<Orders[]>({
         queryFn: async () => {
             const { data } = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/all-orders-users/${uuid}`)
             return data;
@@ -13,7 +43,7 @@ export const useAllProductsUser = (uuid: string) => {
 }
 // Use Get All Approved Products
 export const useToShipProductsUsers = (uuid: string) => {
-    const { data, isLoading, error, isError } = useQuery({
+    const { data, isLoading, error, isError } = useQuery<Orders[]>({
         queryFn: async () => {
             const { data } = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/to-ship/${uuid}`)
             return data;
@@ -25,7 +55,7 @@ export const useToShipProductsUsers = (uuid: string) => {
 }
 // Use Get All Approved Products
 export const useToReceivedProductsUsers = (uuid: string) => {
-    const { data, isLoading, error, isError } = useQuery({
+    const { data, isLoading, error, isError } = useQuery<Orders[]>({
         queryFn: async () => {
             const { data } = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/to-received/${uuid}`)
             return data;
